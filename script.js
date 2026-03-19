d242e7679706de75c017f890380ee3944187be4c
Romulo cComp <romuloccomp@gmail.com>
2026-03-19 17:11:23 -0300

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+// TODO: Implemente a função avaliarCandidato() aqui
+// Dicas:
+// 1. Pegue os valores dos inputs usando getElementById() e .value
+// 2. Converta os valores para número usando Number()
+// 3. Declare variáveis para armazenar os valores
+// 4. Use if/else para verificar os critérios
+// 5. Atualize o parágrafo com id="resultado" usando innerHTML
+// 6. Adicione a classe apropriada (.apto ou .inapto) usando classList.add()
+
+function avaliarCandidato() {
+    // Seu código aqui...
+}
